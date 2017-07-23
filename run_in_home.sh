651596250b4fe794d1e3268759089d6731b85a41
Chris Bayley <chris@codeweaver.co.nz>
2017-07-24 10:57:08 +1200

    updated

--- run_in_home.sh
+++ run_in_home.sh
@@ -12,26 +12,26 @@ if [ "$EUID" == "0" ]
   esac
 fi
 
-# sudo apt-get update
-# sudo apt-get upgrade
-# sudo apt-get autoremove
-# sudo apt-get clean
-#
-# bash <(curl -sL https://raw.githubusercontent.com/node-red/raspbian-deb-package/master/resources/update-nodejs-and-nodered)
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get autoremove
+sudo apt-get clean
 
-# sudo npm i -g --unsafe-perm  node-red-dashboard
-# sudo npm i -g --unsafe-perm  node-red-contrib-xbee
-# # sudo npm i -g serialport
-# sudo npm i -g --unsafe-perm  xbee-api
-#
+bash <(curl -sL https://raw.githubusercontent.com/node-red/raspbian-deb-package/master/resources/update-nodejs-and-nodered)
+
+sudo npm i -g --unsafe-perm  node-red-dashboard
+sudo npm i -g --unsafe-perm  node-red-contrib-xbee
+# sudo npm i -g serialport
+sudo npm i -g --unsafe-perm  xbee-api
+
 
 sudo systemctl enable nodered
 
 sudo apt-get install git
 git config --global alias.st status
 git config --global alias.ci commit
-# git config --global user.name "Chris BAYLEY"
-# git config --global user.email "chris@codeweaver.co.nz"
+git config --global user.name "Chris BAYLEY"
+git config --global user.email "chris@codeweaver.co.nz"
 git config --global color.ui always
 git config --global push.default current
 git config --global log.decorate full
@@ -41,23 +41,12 @@ git config --global branch.autosetuprebase always
 ssh-keygen
 cat .ssh/id_rsa.pub
 
-# mkdir .node-red
-# cd .node-red/
-# git clone git@github.com:chrisbayley/WaterSys104EVR.git
+## get a node-red project into .node-red
 git clone https://github.com/chrisbayley/WaterSys104EVR.git .node-red
 
-# cd
-#mkdir nodes
-#cd nodes
-#git clone git@github.com:chrisbayley/NR-XBeeAPI.git
-#git clone https://github.com/chrisbayley/NR-XBeeAPI.git
-
+## disable some unneeded services
 sudo systemctl disable avahi-daemon
 sudo systemctl disable serial-getty@ttyAMA0.service
-
-## sort perms on serial port
-#sudo adduser pi tty
-#sudo chmod g+r /dev/ttyS0
 
 ## install minimal Xserver
 sudo apt-get install --yes --no-install-recommends xserver-xorg
@@ -76,7 +65,11 @@ ln -sf kiosk.sh .Xsession
 # sudo nano Xwrapper.config
 sudo sh -c "echo -ne '\nallowed_users=anybody\n' >> /etc/X11/Xwrapper.config"
 
-
+# sort out some device permissions
 sudo sh -c 'echo SUBSYSTEM==\"backlight\", GROUP=\"video\" > 50-backlight.rules'
 
-# sudo reboot
+## sort perms on serial port
+#sudo adduser pi tty
+#sudo chmod g+r /dev/ttyS0
+
+echo "DONE: Now so 'sudo reboot'"
