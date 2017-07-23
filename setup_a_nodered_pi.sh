c00f3c2b83ccf6c047a23e52d4a7aa5d537a29de
Chris Bayley <chris@codeweaver.co.nz>
2017-07-23 19:52:51 +1200

    update setup pi.sh

--- setup_a_nodered_pi.sh
+++ setup_a_nodered_pi.sh
@@ -14,43 +14,47 @@ fi
 
 
 # bash <(curl -sL https://raw.githubusercontent.com/node-red/raspbian-deb-package/master/resources/update-nodejs-and-nodered)
-### TESTED MARKER - lines before here tested #####
 
 ## remove the kernel serial console
-sed -ibak -re 's/console=serial0,[0-9]+ //' /boot/cmdline.txt
+# sed -ibak -re 's/console=serial0,[0-9]+ //' /boot/cmdline.txt
+#
+# ## Enable the ttyS0 for general use
+# CONFIG_FILE="/boot/config.txt"
+# UART_TXT="enable_uart"
+# if grep --silent ${UART_TXT} $CONFIG_FILE
+# then
+# 	sed -i.bak -e "s/^\(${UART_TXT}\)=./\1=1/" $CONFIG_FILE
+# else
+# 	echo -en "\n# Enable the serial port\n${UART_TXT}=1\n" >> $CONFIG_FILE
+# fi
 
-## Enable the ttyS0 for general use
-CONFIG_FILE="/boot/config.txt"
-UART_TXT="enable_uart"
-if grep --silent ${UART_TXT} $CONFIG_FILE
-then
-	sed -i.bak -e "s/^\(${UART_TXT}\)=./\1=1/" $CONFIG_FILE
-else
-	echo -en "\n# Enable the serial port\n${UART_TXT}=1\n" >> $CONFIG_FILE
-fi
-
+### TESTED MARKER - lines before here tested #####
 
 # systemctl enable nodered
 #
-# apt-get install git
-# git config --global alias.st status
-# git config --global alias.ci commit
+apt-get install git
+git config --global alias.st status
+git config --global alias.ci commit
 # git config --global user.name "Chris BAYLEY"
 # git config --global user.email "chris@codeweaver.co.nz"
-# git config --global color.ui always
-# git config --global push.default current
-# git config --global log.decorate full
-# git config --global branch.rebase true
-# git config --global branch.autosetuprebase always
-#
-# ssh-keygen
-# cat .ssh/id_rsa.pub
-# cd .node-red/
+git config --global color.ui always
+git config --global push.default current
+git config --global log.decorate full
+git config --global branch.rebase true
+git config --global branch.autosetuprebase always
+
+ssh-keygen
+cat .ssh/id_rsa.pub
+cd .node-red/
 #
 # git clone git@github.com:chrisbayley/WaterSys104EVR.git
-# mkdir nodes
-# cd nodes
-# git clone git@github.com:chrisbayley/NR-XBeeAPI.git
+git clone https://github.com/chrisbayley/WaterSys104EVR.git
+mkdir nodes
+cd nodes
+#git clone git@github.com:chrisbayley/NR-XBeeAPI.git
+git clone https://github.com/chrisbayley/NR-XBeeAPI.git
+
+
 # vi settings.js
 # bash <(curl -sL https://raw.githubusercontent.com/node-red/raspbian-deb-package/master/resources/update-nodejs-and-nodered)
 # sudo npm i -g serialport
